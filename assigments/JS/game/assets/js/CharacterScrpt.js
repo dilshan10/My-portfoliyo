81e5b28aa1351e9afd53a21576ab304907531dac
dilshan10 <superdilshan999@gmail.com>
2022-11-30 21:29:30 +0530

    add character idia Animation

--- assigments/JS/game/assets/js/CharacterScrpt.js
+++ assigments/JS/game/assets/js/CharacterScrpt.js
@@ -1,15 +1,41 @@
-$(document).keydown(function (event){
-    if (event.key == 'd'|| event.key == 'D' || event.key == 'ArrowRight'){
-        console.log(event.key)
-        $('#CHARACTER').animate([
-            {
-                transform: 'height : 100px',
-                transform: 'width : 100px'
-            }
+var idleImgNum = 0;
+let idleInterval = 0;
 
-        ],{
-            duration : 100,
-            iterations: Infinity
-        });
+// function for character idle
+function idleCharacter() {
+    idleImgNum++;
+
+    if (idleImgNum == 10) {
+        idleImgNum = 1;
     }
-})
+
+    $('#character').attr('src', "../assets/cracter moments/png/Idle__00" + idleImgNum + ".png");
+}
+
+// start Idle Animation
+function startIdleAnimation() {
+    idleInterval = setInterval(idleCharacter, 250);
+}
+////////////////////////////////////////////////////////////////////
+var MarginLeft=1540;
+function setBarriers(){
+
+    for (let i = 0; i <= 10; i++) {
+        var barrier=document.createElement("div");
+        barrier.className="barrier";
+        document.getElementById("background").appendChild(barrier);
+        barrier.style.marginLeft = MarginLeft+"px";
+
+        barrier.id="barrier"+i;
+
+        MarginLeft=MarginLeft+1000;
+
+        if (i<5){
+            MarginLeft=MarginLeft+2000;
+        }
+
+        if (i>=5){
+            MarginLeft=MarginLeft+1000;
+        }
+    }
+}
